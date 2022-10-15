2ff3bdae0cb4c49d927eec759253c295f31b7597
Wallace Camargo <93130649+wlcamargo@users.noreply.github.com>
2022-10-15 16:13:07 -0300

    Update DBA - Truncate em Todas as Tabelas do Banco com Exceção.sql

--- DBA/DBA - Truncate em Todas as Tabelas do Banco com Exceção.sql
+++ DBA/DBA - Truncate em Todas as Tabelas do Banco com Exceção.sql
@@ -1,18 +1,16 @@
-
-
+use PRD_ESCOLA
+go
 
 Declare @Tabelas Table (Idx Int Identity(1,1), TblName Varchar(100))             
-
 Insert into @Tabelas (TblName)    
 
--- Clausula da Exceção na tabela que não deseja fazer truncate --> TABLE_NAME
 Select 
 	Table_Name 
 From 
 	Information_Schema.Tables  
 Where 
 	Table_Type = 'Base Table' 
-	and Table_NAME <> 'FATO_MOVIMENTACAO_ITEM'           
+	and Table_NAME <> 'sysssislog'           
      
 Declare @Start Int             
 Declare @End Int             
